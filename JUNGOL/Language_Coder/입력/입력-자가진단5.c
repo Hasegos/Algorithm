bd80ff1e7d1df0cf9daad6dd506a1821d3bb192f
choi su ho <csw020106@naver.com>
2022-02-27 20:55:13 +0900

    입력 - 자가진단5

---
+++ JUNGOL/Language_Coder/입력/입력-자가진단5.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void)
+{
+    double yd1 = 91.44 , in1 =2.54;
+    printf(" 2.1yd = %.1lfcm\n",yd1 *2.1);
+    printf("10.5in =  %.1lfcm",in1 * 10.5);
+    return 0;
+}
