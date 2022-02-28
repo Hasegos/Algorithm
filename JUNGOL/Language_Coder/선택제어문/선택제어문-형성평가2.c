a5fb900f80489bf6812f356e83e13cf7ec11f577
choi su ho <csw020106@naver.com>
2022-03-01 03:13:05 +0900

    선택제어문 - 형성평가2

--- JUNGOL/Language_Coder/선택제어문/선택제어문-형성평가2.c
+++ JUNGOL/Language_Coder/선택제어문/선택제어문-형성평가2.c
@@ -1,7 +1,22 @@
 #include<stdio.h>
-
+/*
+    정수를 입력받아 0 이면 "zero" 양수이면 "plus" 음수이면 "minus" 라고 출력하는 프로그램을 작성하시오.
+*/
 int main(void)
 {
-    
+    int num;
+    scanf("%d",&num);
+    if(num == 0)
+    {
+        printf("zero");
+    }
+    else if(num > 0)
+    {
+        printf("plus");
+    }
+    else
+    {
+        printf("minus");
+    }
     return 0;
 }
