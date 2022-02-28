09637b51bf42801e75815f24a83006f6eb533abf
choi su ho <csw020106@naver.com>
2022-03-01 03:13:24 +0900

    선택제어문 - 형성평가3

--- JUNGOL/Language_Coder/선택제어문/선택제어문-형성평가3.c
+++ JUNGOL/Language_Coder/선택제어문/선택제어문-형성평가3.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
-
+/*
+    년도를 입력받아 윤년(leap year)인지 평년(common year)인지 판단하는 프로그램을 작성하시오.
+*/
 int main(void)
 {
-    
+    int year;
+    scanf("%d",&year);
+    if(year%400 == 0 || (year%4 == 0 && year%100 != 0))
+    {
+        printf("leap year");
+    }
+    else
+    {
+        printf("common year");
+    }
     return 0;
 }
