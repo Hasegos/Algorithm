439c2780c6e762ca87f30876888f64ebf0f9894a
choi su ho <csw020106@naver.com>
2022-03-01 03:12:51 +0900

    선택제어문 - 형성평가1

--- JUNGOL/Language_Coder/선택제어문/선택제어문-형성평가1.c
+++ JUNGOL/Language_Coder/선택제어문/선택제어문-형성평가1.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
-
+/* 
+    두 개의 정수를 입력받아 큰 수에서 작은 수를 뺀 차를 출력하는 프로그램을 작성하시오.
+*/
 int main(void)
 {
-    
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a > b)
+    {
+        printf("%d",a - b);
+    }
+    else 
+    {
+        printf("%d",b - a);
+    }
     return 0;
 }
