df17ae468d8437064147c268423109776161edba
choi su ho <csw020106@naver.com>
2022-03-01 20:32:34 +0900

    반복제어문2 - 자가진단2

---
+++ JUNGOL/Language_Coder/반복제어문2/반복제어문2-자가진단2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/*
+    10부터 20까지의 숫자를 차례대로 출력하는 프로그램을 작성하시오.for문을 사용하세요.
+*/
+int main(void)
+{
+    for(int i = 10; i <=20; i++)
+    {
+        printf("%d ",i);
+    }
+    return 0;
+}
