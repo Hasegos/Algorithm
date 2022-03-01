e0e849da85edf51737f4fcea7086b0885bc363b9
choi su ho <csw020106@naver.com>
2022-03-01 14:20:33 +0900

    반복제어문1 - 형성평가4

---
+++ JUNGOL/Language_Coder/반복제어문1/반복제어문1-형성평가4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/*
+    0 이 입력될 때까지 정수를 계속 입력받아 3의 배수와 5의 배수를 제외한 수들의 개수를 출력하는 프로그램을 작성하시오.
+*/
+int main(void)
+{
+    int num,a = 0;
+    while (1)
+    {
+        scanf("%d",&num);
+        if(num == 0)
+        {
+            break;
+        }
+        else if(num % 3 !=0 && num % 5 !=0 && num != 0)
+        {
+            a++;
+        }
+    }
+    printf("%d",a);    
+    return 0;
+}
